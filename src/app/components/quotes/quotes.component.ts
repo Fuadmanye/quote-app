f301875d1dde18fe7409665967f10f6ff6dc0952
fuad manye / <fahadbasi333@gmail.com>
2022-04-11 11:22:54 +0300

    fix quotes duplication

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -14,7 +14,8 @@ export class QuotesComponent implements OnInit {
         new Quotes('Business opportunities are like buses, theres always another one coming.','Richard Branson','Faith',new Date(2020,3,30),4,3),
         new Quotes('Life is an awful, ugly place to not have a best friend','Dessen Sarah', 'Raymo',new Date(2019,7,23),3,4),
         new Quotes('The future belongs to those who believe in the beauty of their dreams.','Franklin Benson', 'Mercy',new Date(2021,4,10),1,0),
-        
+        new Quotes('Believe that life is worth living and your belief will help create the fact.','Wiliam James', 'Maguire',new Date(2010,4,10),3,0),
+        new Quotes('Don’t focus on negative things; focus on the positive, and you will flourish.','Alek Wek', 'Mary',new Date(1998,4,10),0,0),
     ]
 
      //function for ading a new quote
